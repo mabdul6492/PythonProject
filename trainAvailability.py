4da5a5bb8b7efd2d37a0e38bfc57eec4e94d4d15
mabdul6492 <59756595+mabdul6492@users.noreply.github.com>
2020-03-15 20:28:05 +0530

    Add files via upload

--- trainAvailability.py
+++ trainAvailability.py
@@ -1,19 +1,13 @@
-# from requests_html import HTMLSession
-# from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 from datetime import date
-
-
-# session = HTMLSession()
-# respons = session.get().html
 
 
 class CheckingBot():
 
     def __init__(self, train_number, date, class_):
         self.train_number = train_number
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.PhantomJS()
         self.date = date
         self.class_ = class_
 
@@ -27,6 +21,27 @@ class CheckingBot():
             return True
         except:
             return False
+
+    def fromStations(self, fromStationNo):
+        driver = self.driver
+
+        driver.find_element_by_id('mat-select-0').click()
+        fromStation = driver.find_element_by_xpath(
+            f'//mat-option[@id="mat-option-{fromStationNo}"]/span')
+        fromStationName = fromStation.text
+        fromStation.click()
+
+        return fromStationName
+
+    def toStations(self, toStationNo):
+        driver = self.driver
+
+        driver.find_element_by_id('mat-select-1').click()
+        toStation = driver.find_element_by_xpath(
+            f'//mat-option[@id="mat-option-{toStationNo}"]/span')
+        toStationName = toStation.text
+        toStation.click()
+        return toStationName
 
     def checkAvailability(self):
         date = self.date
@@ -85,8 +100,8 @@ def takeInput():
         return train_number, total_days, class_, travel_date, travel_month
     except:
         print('\nIncorrect data\nTry Again\n')
-        train_number, total_days, class_ = takeInput()
-        return train_number, total_days, class_
+        train_number, total_days, class_, travel_date, travel_month = takeInput()
+        return train_number, total_days, class_, travel_date, travel_month
 
 
 if __name__ == "__main__":
@@ -98,13 +113,30 @@ if __name__ == "__main__":
     checking = CheckingBot(train_number, total_days, class_)
     print("Waiting for the serve's response...")
     status = checking.checkConnection()
+    fromStationNo = 0
+    toStationNo = 74
     if status:
         print('Connection Established')
-        info = checking.checkAvailability()
-        print('Date\tAvailability')
-        [print(f'{travel_date - 2 + x}/{travel_month}\t{info[x]}')
-         for x in range(6)]
-        checking.closeBrowser()
+        while True:
+            try:
+                fromStationName = checking.fromStations(fromStationNo)
+                fromStationNo += 1
+                while True:
+                    try:
+                        toStationName = checking.toStations(toStationNo)
+                        toStationNo += 1
+                        info = checking.checkAvailability()
+                        print(
+                            f'\nFrom Station : {fromStationName} to Station : {toStationName}\nDate\tAvailability\n')
+                        [print(f'{travel_date - 2 + x}/{travel_month}\t{info[x]}')
+                         for x in range(6)]
+                    except Exception as e:
+                        print(e)
+                        break
+            except Exception as e:
+                print(e)
+                break
     else:
         print('Server not Responding')
-        checking.closeBrowser()
+
+    checking.closeBrowser()
